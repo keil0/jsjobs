371b1d280cbdc1254876663b84d073a9a1b09e33
Pierre Cruz <contact@pierrecruz.com>
2018-02-13 22:21:32 +0100

    Display jobs title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { JobListComponent } from './job-list/job-list.component';
+
+import { JobService } from './services/job.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    JobListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    JobService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/job-list/job-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { JobService } from '../services/job.service';
+
+@Component({
+  selector: 'cc-job-list',
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.css']
+})
+export class JobListComponent implements OnInit {
+
+  jobs = [];
+  error = '';
+
+  constructor(private jobService:JobService) { }
+
+  ngOnInit() {
+    this.jobService.getJobs().subscribe(data => {
+      this.jobs = data
+    }, error => { 
+      console.error(error);
+      this.error = error;
+    });
+  }
+
+}
