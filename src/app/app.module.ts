7e37c25d93966e8f257c9a556c85f687831d2e7b
Pierre Cruz <contact@pierrecruz.com>
2018-02-13 22:41:30 +0100

    Create job add form template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
+
+import { JobService } from './services/job.service';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { JobListComponent } from './job-list/job-list.component';
-
-import { JobService } from './services/job.service';
+import { JobAddFormComponent } from './job-add-form/job-add-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    JobListComponent
+    JobListComponent,
+    JobAddFormComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [
     JobService
